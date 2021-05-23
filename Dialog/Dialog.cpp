4248d6950fd178416adcd8bbf4cdebf5a5bc52c2
PeturCh <petur.chuklev@gmail.com>
2021-05-23 14:16:49 +0300

    add help

--- Dialog/Dialog.cpp
+++ Dialog/Dialog.cpp
@@ -1,5 +1,30 @@
 #include "..\Commands\commands.cpp"
 
+void getHelp()
+{
+    std::cout<<"The following commands are supported:\n"
+            <<"------------------------FILES------------------------"
+            <<"\nopen <file>	            opens <file>\n"
+            <<"\nclose			    closes currently opened file\n"
+            <<"\nsave			    saves the currently open file\n"
+            <<"\nsaveas <file>	            saves the currently open file in <file>\n"
+            <<"\nhelp			    prints this information\n"
+            <<"\nexit			    exists the program\n"
+            <<"\n---------------------Jedi & Planets---------------------"
+            <<"add_planet <planet name>\n"
+            <<"\ncreate_jedi  <planet name> <jedi name> <rank> <age> <saber colour> <power>\n"
+            <<"\nremove_jedi  <jedi name> <planet name>\n"
+            <<"\npromote_jedi  <jedi name> <multiplier>\n"
+            <<"\ndemote_jedi  <jedi name> <multiplier>\n"
+            <<"\nget_strongest_jedi  <planet name>\n"
+            <<"\nget_youngest_jedi  <planet name> <rank>\n"
+            <<"\nget_most_used_saber_color  <planet name> <rank>\n"
+            <<"\nget_most_used_saber_color  <planet name>\n"
+            <<"\nprint_jedi  <jedi name>\n"
+            <<"\nprint_planet  <planet name>\n"
+            <<"\n<planet name> + <planet name>  prints jedi from both planets\n";
+}
+
 //std::ifstream open(char *fileName)
 //{
 //    std::ifstream input(fileName);
@@ -15,8 +40,23 @@ int main()
         std::fill(std::begin(command), std::end(command), '\0');
         std::cout<<"\n> ";
         std::cin>>command;
+        
+        if(strcmp(command, "open") == 0)
+            std::cout<<"openCalled\n";
+            
+        else if(strcmp(command, "save") == 0)
+            std::cout<<"saveCalled\n";
 
-        if(strcmp(command, "add_planet") == 0)
+        else if(strcmp(command, "saveas") == 0)
+            std::cout<<"saveasCalled\n";
+
+        else if(strcmp(command, "close") == 0)
+            std::cout<<"closeCalled\n";
+
+        else if(strcmp(command, "help") == 0)
+            getHelp();
+
+        else if(strcmp(command, "add_planet") == 0)
             {
                 char planetName[50];
                 std::cin>>planetName;
@@ -110,7 +150,8 @@ int main()
                 std::cin>>plananetName2;
                 printJediFromPlanets(command, plananetName2);
             }
+            std::cin.ignore(256, '\n');
         }
         
-    } while (strcmp(command, "Exit") != 0);
+    } while (strcmp(command, "exit") != 0);
 }
